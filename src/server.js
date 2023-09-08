6d82b93aad583bc5ea88abd1cbb2f058fc296edf
RenanS4ntos <renan-bs@hotmail.com>
2023-09-08 12:53:13 -0300

    Streams no Node.js - Criando middleware de JSON

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 /**
  * - Criar usuário
@@ -27,20 +28,21 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
   const {method, url} = req
 
+  await json(req,res)
+
   if(method === 'GET' && url === '/users') {
-    return res
-        .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
+    return res.end(JSON.stringify(users))
   }
   
   if(method === 'POST' && url === '/users') {
+    const {name, email} = req.body
     users.push({
-      id: 1,
-      name: 'Teste',
-      email: 'teste@example.com'
+      id: users.length + 1,
+      name,
+      email,
     })
     return res.writeHead(201).end()
   }
