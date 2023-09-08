42ea445f4442505a330859136e278444755650aa
Renan Santos <renansnts@pop-os.localdomain>
2023-09-08 11:36:13 -0300

    Streams no Node.js - Criando stream de leitura

--- src/server.js
+++ src/server.js
@@ -1,7 +1,51 @@
 import http from 'node:http';
 
+/**
+ * - Criar usuário
+ * - Listar usuário
+ * - Editar usuário
+ * - Deletar usuário
+ * 
+ * 
+ * - HTTP
+ *  - Método HTTP
+ *  - URL
+ * 
+ * GET, POST, PATCH, PUT, DELETE
+ * 
+ * GET - buscar recurso do back-end
+ * POST - criar um recurso no back-end
+ * PATCH - atualiza uma informação específica no back-end
+ * PUT - atualiza um recurso no back-end
+ * DELETE - remove um recurso no back-end
+ * 
+ * Statefull - Stateless
+ * 
+ * Cabeçalhos (Requisições/respostas) => Metadados
+ * HTTP Status Code
+ */
+
+const users = [];
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello world!')
+  const {method, url} = req
+
+  if(method === 'GET' && url === '/users') {
+    return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+  }
+  
+  if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Teste',
+      email: 'teste@example.com'
+    })
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
